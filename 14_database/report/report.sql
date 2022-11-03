f787e806ec8a70468dad7d6b480d54287bb9d367
gitjaeran <devjaeran@gmail.com>
2022-11-03 16:13:48 +0900

    study: mysql 기본

---
+++ 14_database/report/report.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE member DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+
+
+USE member;
+
+
+CREATE TABLE member (
+  id VARCHAR(20) NOT NULL PRIMARY KEY,
+  name VARCHAR(5) NOT NULL,
+  age INT(2),
+  gender VARCHAR(2) NOT NULL,
+  email VARCHAR(50),
+  promotion VARCHAR(2) NOT NULL DEFAULT 'x'
+);
